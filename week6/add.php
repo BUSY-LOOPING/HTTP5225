fd052262b3e33af8f626c366df525d9dfa6a2ccf
BUSY-LOOPING <dhruvyadav2905@gmail.com>
2025-08-14 21:43:49 -0400

    added week6

--- week6/add.php
+++ week6/add.php
@@ -3,7 +3,7 @@
 	secure();
 ?>
 
-<?php
+<?php   
     if(isset($_POST['AddSchool'])){
         $BoardName = $_POST['BoardName'];
         $SchoolName = $_POST['SchoolName'];
@@ -20,7 +20,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
